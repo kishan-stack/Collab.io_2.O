49af47102a1f2705c8ba9d167d4163603f55745e
kishan <varmajaykishan63@gmail.com>
2024-08-07 01:11:46 +0530

    standardize apiresponse, apierror, asyncHandler

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,15 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
-
+import { app } from "./app.js";
 dotenv.config({path:'./env'})
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`✨ Server is running on port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed with error :: ",err)
+})
