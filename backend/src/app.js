3a1529795ef28112ca7939d159ba27bee64c3daa
kishan <varmajaykishan63@gmail.com>
2024-08-08 06:32:57 +0530

    add middlewares for security and other purposes, upgrade http server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,38 @@
 import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser"
 import cors from "cors"
-
+import morgan from "morgan";//http request logger middleware
+import rateLimit from "express-rate-limit"
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize"
+import xss from "xss"
 
 const app = express();
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials: true,
-}))
 
-app.use(express.json({
-    limit: "16kb",
-}));
-app.use(urlencoded({
-    extended: true,
-    limit:"16kb"
-}))
+app.use(cors({origin:process.env.CORS_ORIGIN,methods:["GET","PATCH","PUT","POST","DELETE"],credentials: true,}))
+app.use(express.json({limit: "16kb",}));
+app.use(express.urlencoded({extended: true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(helmet())
+
+if(process.env.NODE_ENV==="development"){
+    app.use(morgan("dev"))
+}
+
+const limiter = rateLimit({
+    max:3000,
+    windowMs:60*60*1000,//for one hour
+    message:"Too many requests, please try again after an hour",
+})
+
+app.use("/tawk",limiter)
+app.use(mongoSanitize())
+// app.use(xss())
+
+
+
+
 
 
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,20 @@ import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 import { app } from "./app.js";
 dotenv.config({path:'./env'})
+import http from 'node:http'
+
+
+const server = http.createServer(app);//upgrading connection for socket io
+
+
 
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000,()=>{
+    // app.listen(process.env.PORT || 8000,()=>{
+    //     console.log(`✨ Server is running on port : ${process.env.PORT}`)
+    // })
+    server.listen(process.env.PORT || 8000,()=>{
         console.log(`✨ Server is running on port : ${process.env.PORT}`)
     })
 })
