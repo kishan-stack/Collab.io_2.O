32fb463d158299ed88a9cd06f74b3dfaef87892e
kishan <varmajaykishan63@gmail.com>
2024-08-08 07:56:33 +0530

    handle edge cases , upgrade database connection method

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import { DB } from "../index.js";
 
+const DBURL = `${DB}/${DB_NAME}`;
 const connectDB = async ()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(DBURL,{
+            useNewUrlParser: true,
+            useCreateIndex:true,
+            useFindAndModify:false,
+            useUnifiedTopology: true,
+        })
         // console.log(connectionInstance)
         console.log(`✨ MongoDb connected || DB host : ${connectionInstance.connection.host}`)
     } catch (error) {
--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,25 @@ const server = http.createServer(app);//upgrading connection for socket io
 
 
 
+//edge cases 
+process.on("uncaughtException",(err)=>{
+    console.error(err)
+    process.exit(1)
+})
 
+process.on("unhandledRejection",(err)=>{
+    console.log(err)
+    server.close(()=>{
+        process.exit(1)
+    })
+})
+//db configuration
+export const DB = process.env.MONGODB_URI.replace(
+  "<PASSWORD>",
+  process.env.MONGODB_PASSWORD
+);
+
+//database connection
 connectDB()
 .then(() => {
     // app.listen(process.env.PORT || 8000,()=>{
